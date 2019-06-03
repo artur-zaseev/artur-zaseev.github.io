d1c9dceb282e22cb413bffd0c83c57c73a42aa53
Артур Засеев <37107439+artur-zaseev@users.noreply.github.com>
2019-06-03 19:09:23 +0300

    Add files via upload

---
+++ assets/inc/header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="/assets/img/favicon.png">
+
+    <title><?php echo $title; ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/style.css" rel="stylesheet">
+  </head> 
+  <body>
---
+++ lab/index.php
@@ -0,0 +1,25 @@
+<?php $title = "Front-End Эксперименты"; ?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/assets/inc/header.php') ?>
+
+<div class="content">
+  <div class="container">
+    
+    <div class="row">
+      <div class="col-12">
+        <h1 class="mt-5 mb-5 text-center">Front-End Эксперименты</h1>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-10 offset-1 col-md-6 offset-md-3">
+        <ul class="list-group">
+          <li class="list-group-item"><a href="/lab/converter/">Конвертер валют</a></li>
+          <li class="list-group-item"><a href="/lab/utm-generator/">Генератор UTM-меток</a></li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/assets/inc/footer.php') ?>
