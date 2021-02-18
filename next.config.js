7efafab93c67231e4f102669c4ed8e4b3dbf98b5
zaseevav <zaseevav@yandex.ru>
2021-02-18 11:31:25 +0300

    clear master branch

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd
-    ? "https://raw.githubusercontent.com/artur-zaseev/artur-zaseev.github.io/master/docs/"
-    : "",
-};
