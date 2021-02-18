70f158cd88f4f9a1adf3d3269a6300e762ebaf83
zaseevav <zaseevav@yandex.ru>
2021-02-18 11:11:56 +0300

    test prefix deploy

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   // Use the CDN in production and localhost for development.
-  assetPrefix: isProd
-    ? "https://raw.githubusercontent.com/artur-zaseev/artur-zaseev.github.io/master/docs/"
-    : "",
+  assetPrefix: isProd ? "/artur-zaseev.github.io/" : "",
 };
