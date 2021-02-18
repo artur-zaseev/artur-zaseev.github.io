787a8de42b51d644573180c8a3cd2196d5ff9ec2
zaseevav <zaseevav@yandex.ru>
2021-02-18 11:59:57 +0300

    regen site

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ff99b649ee6af6b201e1";
+/******/ 	var hotCurrentHash = "bc28e7ee4f4719504a55";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
