365676394c3eec62e7f381391d40a3bd208e8961
zaseevav <zaseevav@yandex.ru>
2021-02-18 11:13:31 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? "/artur-zaseev.github.io/" : "",
-};
+// module.exports = {
+//   // Use the CDN in production and localhost for development.
+//   assetPrefix: isProd ? "/artur-zaseev.github.io/" : "",
+// };
